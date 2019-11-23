162c8790adc28d9660908b6759675f3dcef2d5ad
Severin <severin_koch@gmx.de>
2019-11-23 11:48:55 +0100

    adding like and dislike button + restructering

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Quote.css"
 
-function Quote(props) {
-  console.log("what is inside props", props.quotes);
+class Quote extends Component {
+  state = {
+            Color: ""
+  };
 
-  return (
-    <div>
-      {props.quotes.map((quotes, key) => {
-        return (
-          <div key={key}>
-            <p>{quotes.quoteText}</p>
-            <h3>{quotes.quoteAuthor}</h3>
-            <hr></hr>
-          </div>
-        );
-      })}
-    </div>
-  );
+
+  handleClick = (event) => {
+    this.setState({
+        
+        Color: "red"
+    });
+  };
+
+  handleClick1 = (event) => {
+    this.setState({
+        
+        Color: "green"
+    });
+  };
+
+
+
+
+  render() {
+    return (
+      <div>
+       
+          
+            
+              <p style={{backgroundColor: this.state.Color}} >{this.props.paragraph}</p>
+              <h3>By: {this.props.author}</h3>
+              <button onClick={this.handleClick1}>like</button>
+              <button onClick={this.handleClick}>dislike</button>
+              <hr></hr>
+          
+         
+      </div>
+    );
+  }
 }
 
 export default Quote;
--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -3,37 +3,59 @@ import Quote from "./Quote";
 
 class QuoteSearcher extends Component {
   state = {
-    quotes: []
+    quotes: [],
+    loading: true,
+    like: null
   };
 
   componentDidMount() {
     fetch(`https://quote-garden.herokuapp.com/quotes/search/tree`)
       .then(data => data.json())
       .then(json => this.storeData(json.results))
-      
 
       .catch(console.error);
-  } 
-
-  storeData(json){
-    console.log("storeData function", json)
-    this.setState({quotes: json})
-
   }
 
+  storeData(json) {
+    console.log("storeData function", json);
+    this.setState({ quotes: json });
+  }
 
-
-
+  handleClick = event => {
+    this.setState({});
+  };
 
   render() {
-
-        console.log("state", this.state.quotes)
-
-
+    const status = this.state.loading;
+    console.log("Datatype of Quotes", typeof this.state.quotes);
+    console.log("Datatype of Quotes", this.state.quotes);
 
     return (
       <div>
-        <Quote quotes={this.state.quotes} />
+        {!status && "loading"}
+        {status &&
+          this.state.quotes.map((element,key) => {
+
+
+
+
+            return (
+
+                <div key={key}>
+              <Quote
+                author={element.quoteAuthor}
+                paragraph={element.quoteText}
+              />
+                </div>
+
+            );
+
+
+
+
+
+
+          })}
       </div>
     );
   }
